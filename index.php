16623c33adcce055ad7b5eef18f74309f8ac7a5c
JoseM021 <122744449+JoseM021@users.noreply.github.com>
2023-11-01 16:09:09 -0500

    Diseño preload animacion

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="/css/preloaderIndex.css">
+    <title>Veterinaria</title>
 </head>
 <body>
     <?php
@@ -32,5 +33,11 @@
             </div>
         </div>
     </header>
+
+    <div class="welcome__message">
+        <div class="welcome__icon">
+            <img src="img/Logo_Veterinary.png" alt="welcome-icon">
+        </div>
+    </div>
 </body>
 </html>
